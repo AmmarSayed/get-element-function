e5a4cb5c9a5ad4154b297974eefc6c012b981213
Ammar Sayed <45734003+AmmarSayed@users.noreply.github.com>
2020-10-25 16:44:17 +0200

    Update getElement.js
    Updated the way function export the list, as originally it was not allowing emmet in vscode to automatically give suggestions such as .addEventListener to the exported element

--- getElement.js
+++ getElement.js
@@ -6,17 +6,18 @@
  * @param {HTMLBodyElement} selection - Select single element by class name, Id, or tag name.
  * @param {Boolean} all - true / 1 / "all" to select all. The Function will return an array of elements. Leave it blank for single selection, or write 0.
  */
-
 const getElement = (selection, all = 0) => {
-  let element = '';
-  if (all) {
-    element = Array.from(document.querySelectorAll(selection));
+  if (selection && !all) {
+    const element = document.querySelector(selection);
+    if (element) return element;
+    throw new Error('no such element exist');
+  } else if (selection && all) {
+    const Elements = Array.from(document.querySelectorAll(selection));
+    if (Elements.length > 0) return Elements;
+    throw new Error('no such elements exist');
   } else {
-    element = document.querySelector(selection);
+    throw new Error('no element selected');
   }
-  if (!element || element.length === 0) throw new Error('no element selected');
-
-  return element;
 };
 
 export default getElement;
